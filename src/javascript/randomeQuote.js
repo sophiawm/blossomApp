88f990c9b8d6396a079d72dbdeeb43fdca0e3f75
Sophia WM <claudiasophiawong@gmail.com>
2023-08-25 09:20:43 +0200

    update generator

---
+++ src/javascript/randomeQuote.js
@@ -0,0 +1,36 @@
+const generateQuote = function() {
+    const quotes = [
+    {
+        quote: "Remember purposeful action",
+    },
+    {
+        quote: "Authenticity is rebellion",
+    },
+    {
+        quote: "Unleash your monster",
+    },
+    {
+        quote: "Grit fuels your journey",
+    },
+    {
+        quote: "You are always worthy",
+    },
+    {
+        quote: "Blame the system",
+    },
+    {
+        quote: "Show up with empathy",
+    },
+    {
+        quote: "Embrace the chaos",
+    }
+];
+
+    let arrayIndex = Math.floor(Math.random() * quotes.length);
+    document.getElementById("phrases-text").innerHTML = quotes[arrayIndex].quote;
+}
+
+window.onload = function() {
+    generateQuote();
+    document.getElementById("change-phrases").addEventListener('click', generateQuote);
+} 
